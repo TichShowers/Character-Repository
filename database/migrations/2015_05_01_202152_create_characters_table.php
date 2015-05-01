65789a259a913d19ff2d9f675c6c72a8cac3609c
Tich Showers <tichsc2@gmail.com>
2015-05-02 01:00:06 +0200

    Initial Commit

---
+++ database/migrations/2015_05_01_202152_create_characters_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharactersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('characters', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->string('image');
+			$table->timestamps();
+            $table->integer('category_id');
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('cascade');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('characters');
+	}
+
+}
---
+++ database/migrations/2015_05_01_202257_create_social_links_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialLinksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('social_links', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('social_links');
+	}
+
+}
